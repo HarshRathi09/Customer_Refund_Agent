d1f14b529f818a282d3e498a588e94ab43c6847f
Harsh Rathi <harshrathi09@gmail.com>
2026-06-25 17:04:16 +0530

    Update seed.py

--- backend/data/seed.py
+++ backend/data/seed.py
@@ -29,6 +29,76 @@ customers = [
         refund_count=0
     )
 
+        Customer(
+        customer_id="C004",
+        name="Richard Morris",
+        refund_count=1
+    ),
+
+    Customer(
+        customer_id="C005",
+        name="Priya Sharma",
+        refund_count=4
+    ),
+
+    Customer(
+        customer_id="C006",
+        name="Kim Jong",
+        refund_count=0
+    ),
+
+     Customer(
+        customer_id="C007",
+        name="Alex Hales",
+        refund_count=3
+    ),
+
+    Customer(
+        customer_id="C008",
+        name="Harry Cooper",
+        refund_count=2
+    ),
+
+    Customer(
+        customer_id="C009",
+        name="M.Leornard",
+        refund_count=0
+    )
+
+        Customer(
+        customer_id="C0010",
+        name="Rahul Desai",
+        refund_count=1
+    ),
+
+    Customer(
+        customer_id="C0011",
+        name="Priya Sharma",
+        refund_count=5
+    ),
+
+    Customer(
+        customer_id="C0012",
+        name="Ali",
+        refund_count=2
+    ), 
+    Customer(
+        customer_id="C0013",
+        name="George Russel",
+        refund_count=3
+    ),
+
+    Customer(
+        customer_id="C0014",
+        name=" Chris Lewis",
+        refund_count=0
+    ),
+
+    Customer(
+        customer_id="C0015",
+        name="Rohith Verma",
+        refund_count=0
+    ),
 ]
 
 orders = [
@@ -55,6 +125,122 @@ orders = [
         order_id="ORD1003",
         customer_id="C003",
         category="Electronics",
+        delivered_date="2026-06-12",
+        opened=False,
+        damaged=True
+    ),
+
+        Order(
+        order_id="ORD1004",
+        customer_id="C004",
+        category="Electronics",
+        delivered_date="2026-06-30",
+        opened=True,
+        damaged=False
+    ),
+
+    Order(
+        order_id="ORD1005",
+        customer_id="C005",
+        category="Clothing",
+        delivered_date="2026-06-15",
+        opened=True,
+        damaged=False
+    ),
+
+    Order(
+        order_id="ORD1006",
+        customer_id="C006",
+        category="Electronics",
+        delivered_date="2026-06-18",
+        opened=False,
+        damaged=True
+    ),
+        Order(
+        order_id="ORD1007",
+        customer_id="C007",
+        category="Furniture",
+        delivered_date="2026-06-09",
+        opened=True,
+        damaged=False
+    ),
+
+    Order(
+        order_id="ORD1008",
+        customer_id="C002",
+        category="Clothing",
+        delivered_date="2026-06-06",
+        opened=True,
+        damaged=True
+    ),
+
+    Order(
+        order_id="ORD1009",
+        customer_id="C009",
+        category="Electronics",
+        delivered_date="2026-06-14",
+        opened=False,
+        damaged=False
+    ),
+
+        Order(
+        order_id="ORD10",
+        customer_id="C010",
+        category="Electronics",
+        delivered_date="2026-06-24",
+        opened=True,
+        damaged=False
+    ),
+
+    Order(
+        order_id="ORD1010",
+        customer_id="C002",
+        category="Clothing",
+        delivered_date="2026-09-02",
+        opened=False,
+        damaged=True
+    ),
+
+    Order(
+        order_id="ORD1011",
+        customer_id="C003",
+        category="Clothing",
+        delivered_date="2026-02-11",
+        opened=False,
+        damaged=True
+    ),
+
+        Order(
+        order_id="ORD12",
+        customer_id="C001",
+        category="Electronics",
+        delivered_date="2026-05-01",
+        opened=True,
+        damaged=False
+    ),
+
+    Order(
+        order_id="ORD1013",
+        customer_id="C002",
+        category="Clothing",
+        delivered_date="2026-06-10",
+        opened=False,
+        damaged=False
+    ),
+
+    Order(
+        order_id="ORD1014",
+        customer_id="C003",
+        category="Electronics",
+        delivered_date="2026-06-12",
+        opened=True,
+        damaged=True
+    ),
+
+     Order(
+        order_id="ORD1015",
+        customer_id="C003",
+        category="Furniture",
         delivered_date="2026-06-12",
         opened=False,
         damaged=True
